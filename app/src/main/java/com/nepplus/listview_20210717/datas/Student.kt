388c6c91d929c5280f816f610e0b6d1f7305b2ed
김태섭 <kyoer234@gmail.com>
2021-07-17 13:56:12 +0900

    학생 클래스의 getAgeByYear 메서드 추가

--- app/src/main/java/com/nepplus/listview_20210717/datas/Student.kt
+++ app/src/main/java/com/nepplus/listview_20210717/datas/Student.kt
@@ -7,6 +7,6 @@ class Student(
 
     //자신의 출생년도를 가지고 -> 나이로 변환해서 경과를 주는 함수를 제작.
     fun getAgeByYear(year : Int) : Int {
-        return year - this.birthYear - 1;
+        return year - this.birthYear + 1;
     }
 }
